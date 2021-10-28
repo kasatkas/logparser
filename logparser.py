fa7e7188e8c357b64c8d49e8d00a52873583d8ae
Alexander Konoplin <konosan9@gmail.com>
2021-10-28 14:39:52 +0300

    Поиск строк

--- logparser.py
+++ logparser.py
@@ -1,14 +1,24 @@
 import sys,os,glob
-arg = sys.argv[1]
+path = sys.argv[1]
+stringToFind = sys.argv[2]
+resultFile =  "E:/result.txt"
 
-if os.path.isdir(arg):
-    for filename in glob.glob(os.path.join(arg, '*.log')):
-        with open(filename, 'r') as f:
-            #do smth
+def findString(file, str, mode):
+    result = open(resultFile, mode)
+    inp = file.readlines()
+    for i in iter(inp):
+        if str in i:
+            #print(i)
+            result.write(i + "\n")
+
+if os.path.isdir(path):
+    for fileName in glob.glob(os.path.join(path, '*.log')):
+        with open(fileName, 'r') as f:
+            findString(f, stringToFind, 'a+')
             f.close()
-elif os.path.isfile(arg):
-    with open(arg, 'r') as f:
-        #do smth
+elif os.path.isfile(path):
+    with open(path, 'r') as f:
+        findString(f, stringToFind, 'w+')
         f.close()
 else: 
     print('Wrong arg!')
