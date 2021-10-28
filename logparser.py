cade3013e3558ef42c10f719e387f41b27ca7725
Alexander Konoplin <konosan9@gmail.com>
2021-10-28 16:18:54 +0300

    Вывод + прогресс бар

--- logparser.py
+++ logparser.py
@@ -1,16 +1,29 @@
+def progressBar(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    total = len(iterable)
+    def printProgressBar (iteration):
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    printProgressBar(0)
+    for i, item in enumerate(iterable):
+        yield item
+        printProgressBar(i + 1)
+    print()
+
 import sys,os,glob
 path = sys.argv[1]
 stringToFind = sys.argv[2]
-resultFile =  "E:/result.txt"
+resultFile =  "E:/result.log"
 
 def findString(file, str, mode):
     result = open(resultFile, mode)
     inp = file.readlines()
-    for i in iter(inp):
+    for i in progressBar(inp, prefix = 'Progress of ' + file.name, suffix = 'Complete', length = 50):
         if str in i:
-            #print(i)
             result.write(i + "\n")
-
+    result.close()
+    
 if os.path.isdir(path):
     for fileName in glob.glob(os.path.join(path, '*.log')):
         with open(fileName, 'r') as f:
@@ -22,4 +35,3 @@ elif os.path.isfile(path):
         f.close()
 else: 
     print('Wrong arg!')
-
